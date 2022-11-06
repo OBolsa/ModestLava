c6e6dd4f1da415593d7bc0656174792bea2a382e
OBolsa <vitor-hug@hotmail.com>
2022-11-06 18:29:53 -0300

    Me parece que o comunismo cresce

--- ModestLarva/Assets/Scripts/GlowUpFoice.cs
+++ ModestLarva/Assets/Scripts/GlowUpFoice.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class GlowUpFoice : MonoBehaviour
 {
-    [SerializeField] private 
+    public float GrowthRate;
+    private Volume m_Volume;
+    private Bloom m_Bloom;
+
+    private void Start()
+    {
+        m_Volume = GetComponent<Volume>();
+        m_Volume.profile.TryGet(out m_Bloom);
+    }
+
+    private void Update()
+    {
+        m_Bloom.dirtIntensity.value += Time.deltaTime * GrowthRate;
+    }
 }
