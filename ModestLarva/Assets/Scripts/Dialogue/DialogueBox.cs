cb74f404dd4180c5f937272d3365e1aa40320778
OBolsa <vitor-hug@hotmail.com>
2022-11-06 14:44:13 -0300

    Sistema de Dialogo

--- ModestLarva/Assets/Scripts/Dialogue/DialogueBox.cs
+++ ModestLarva/Assets/Scripts/Dialogue/DialogueBox.cs
@@ -5,5 +5,35 @@ using TMPro;
 
 public class DialogueBox : MonoBehaviour
 {
+    [SerializeField] private DialogueChannel m_Channel;
     public TMP_Text m_DialogueText;
+    private Dialogue m_Dialogue;
+
+    public IEnumerator StartDialogue(Dialogue setDialogue)
+    {
+        m_Dialogue = setDialogue;
+
+        int lineIndex = 0;
+        int index = 0;
+
+        while(lineIndex < m_Dialogue.Line.Length)
+        {
+            m_DialogueText.text = m_Dialogue.Line[lineIndex].Text[index].Text;
+
+            index++;
+
+            if(index >= m_Dialogue.Line[lineIndex].Text.Length)
+            {
+                index = 0;
+                lineIndex++;
+            }
+
+            if (lineIndex >= m_Dialogue.Line.Length)
+                yield break;
+            else
+                yield return new WaitForSeconds(m_Dialogue.Line[lineIndex].Text[index].TimeInThisText);
+        }
+
+        m_Channel.RaiseCloseDialogue(m_Dialogue);
+    }
 }
